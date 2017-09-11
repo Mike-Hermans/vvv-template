e3c1aea88c60f235685f1e7e43c2f9439fb8fb75
Mike Hermans <mail@mikehermans.nl>
2017-09-11 19:48:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# VVV personal site template
+Copied from https://github.com/Varying-Vagrant-Vagrants/custom-site-template
+
+## Overview
+This template will allow you to create a WordPress dev environment using only `vvv-custom.yml`.
+
+# Configuration
+
+### The minimum required configuration:
+
+```
+my-site:
+  repo: https://github.com/mike-hermans/vvv-template
+  hosts:
+    - my-site.dev
+```
+| Setting    | Value       |
+|------------|-------------|
+| Domain     | my-site.dev |
+| Site Title | my-site.dev |
+| DB Name    | my-site     |
+| Site Type  | Single      |
+| WP Version | Latest      |
+
+## Configuration Options
+
+```
+hosts:
+    - foo.dev
+    - bar.dev
+    - baz.dev
+```
+Defines the domains and hosts for VVV to listen on. 
+The first domain in this list is your sites primary domain.
+
+```
+custom:
+    site_title: My Awesome Dev Site
+```
+Defines the site title to be set upon installing WordPress.
+
+
+```
+custom:
+    db_name: super_secet_db_name
+```
+Defines the DB name for the installation.
+
+
---
+++ provision/vvv-init.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+# Provision WordPress Stable
+
+DOMAIN=`get_primary_host "${VVV_SITE_NAME}".dev`
+DOMAINS=`get_hosts "${DOMAIN}"`
+SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
+WP_VERSION=`get_config_value 'wp_version' 'latest'`
+WP_TYPE=`get_config_value 'wp_type' "single"`
+DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
+DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
+
+# Make a database, if we don't already have one
+echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
+echo -e "\n DB operations done.\n\n"
+
+# Nginx Logs
+mkdir -p ${VVV_PATH_TO_SITE}/log
+touch ${VVV_PATH_TO_SITE}/log/error.log
+touch ${VVV_PATH_TO_SITE}/log/access.log
+
+# Install and configure the latest stable version of WordPress
+if [[ ! -d "${VVV_PATH_TO_SITE}/public_html" ]]; then
+    mkdir public_html
+    cd ${VVV_PATH_TO_SITE}/public_html
+
+    echo "Downloading WordPress..."
+    noroot wp core download --version="${WP_VERSION}"
+
+    echo "Configuring Database and DEBUG mode"
+    noroot wp core config --dbname="${DB_NAME}" --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
+define( 'WP_DEBUG', true );
+PHP
+
+    echo "Installing WordPress"
+    noroot wp core install --url="${DOMAIN}" --quiet --title="${SITE_TITLE}" --admin_name=admin --admin_email="admin@local.dev" --admin_password="admin"
+
+    # Remove unneeded plugins and themes
+    cd wp-content/plugins
+    rm -rf akismet hello.php
+
+    cd ../themes
+    rm -rf twenty*
+
+    # Download and configure theme
+    git clone https://github.com/Mike-Hermans/Clarkson-Theme.git .
+    mv Clarkson-Theme ${VVV_SITE_NAME}
+    noroot wp theme activate ${VVV_SITE_NAME}
+    cd ${VVV_SITE_NAME}
+    composer install
+    cd development
+    npm install
+    bower install
+    gulp --production
+
+else
+    echo "Updating WordPress"
+    cd ${VVV_PATH_TO_SITE}/public_html
+    noroot wp core update --version="${WP_VERSION}"
+fi
+
+cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+sed -i "s#{{DOMAINS_HERE}}#${DOMAINS}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
